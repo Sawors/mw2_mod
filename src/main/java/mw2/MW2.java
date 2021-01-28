9115f226515a7405aec46d4657b6a480154a1f19
Sawors <40705096+Sawors@users.noreply.github.com>
2021-01-28 16:44:16 +0100

    first 1

--- src/main/java/mw2/MW2.java
+++ src/main/java/mw2/MW2.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package mw2;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,8 +7,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = MW2.MODID, name = MW2.NAME, version = MW2.VERSION)
+public class MW2
 {
     public static final String MODID = "examplemod";
     public static final String NAME = "Example Mod";
